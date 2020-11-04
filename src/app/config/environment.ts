9da65ae754c6e72cce3f8ae24711d00da1a4286b
sulguy8 <sulguy8@gmail.com>
2020-11-04 12:47:13 +0900

    URL 수정

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
-  localUrl :  'http://localhost',
-  baseUrl:  'http://localhost:809/',
+  // Localhost
+  // localUrl: 'http://localhost',
+  // baseUrl: 'http://localhost:809/',
+
+  // Live Server
+  localUrl: 'http://zeplin.synology.me/',
+  baseUrl: 'http://zeplin.synology.me:809/',
 } 
