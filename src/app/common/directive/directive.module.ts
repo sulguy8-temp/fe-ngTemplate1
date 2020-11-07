8320d8a237bb5e4995ec26cb41f60a3f171c2ad3
sulguy8 <sulguy8@gmail.com>
2020-11-07 19:19:18 +0900

    form 구현

--- src/app/common/directive/directive.module.ts
+++ src/app/common/directive/directive.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PressDirective } from './press/press.directive';
-import { ErrorDirective } from './error/error.directive';
 
 @NgModule({
   declarations: [
     PressDirective,
-    ErrorDirective
   ],
   imports: [
     CommonModule 
   ],
   exports: [
     PressDirective,
-    ErrorDirective
   ]
 })
 export class DirectiveModule { }
--- src/app/page/main/main/picture4/picture4.component.ts
+++ src/app/page/main/main/picture4/picture4.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-picture4',
@@ -7,20 +7,80 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./picture4.component.css']
 })
 export class Picture4Component implements OnInit {
-  form = new FormGroup({
-    name: new FormControl('', Validators.required),
-    desc: new FormControl('', Validators.required),
-    radio: new FormControl('', Validators.required)
-  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
 
   }
 
-  test(){
-    console.log(this.form)
-    console.log(this.form.status)
+  profileForm2 = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('', Validators.required)
+    ])
+  });
+
+  get aliases() {
+    return this.profileForm2.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control('', Validators.required));
+  }
+
+  onSubmit() {
+    console.warn(this.profileForm2.value);
+  }
+
+  /////////////////////////////////////////////////////////////////////
+
+  hero = { name: '', alterEgo: '', power: '' }
+  heroForm = new FormGroup({
+    name: new FormControl(this.hero.name, [
+      Validators.required,
+      Validators.minLength(4),
+      this.forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
+    ]),
+    alterEgo: new FormControl(this.hero.alterEgo),
+    power: new FormControl(this.hero.power, Validators.required)
+  });
+
+  get name2() { return this.heroForm.get('name'); }
+
+  get power() { return this.heroForm.get('power'); }
+
+  updateProfile() {
+    this.profileForm2.get('lastName').setValue('ysys');
+    this.profileForm2.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const forbidden = nameRe.test(control.value);
+      return forbidden ? {forbiddenName: {value: control.value}} : null;
+    };
   }
 }
+
+// export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+//   return (control: AbstractControl): {[key: string]: any} | null => {
+//     const forbidden = nameRe.test(control.value);
+//     return forbidden ? {forbiddenName: {value: control.value}} : null;
+//   };
+// }
+
